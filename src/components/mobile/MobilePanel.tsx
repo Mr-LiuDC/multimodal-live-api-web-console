ca11a51673ad8c3eada9107ee86bff67749a1740
刘德财 <442823529@qq.com>
2024-12-24 15:10:44 +0800

    Add example video

--- src/components/mobile/MobilePanel.tsx
+++ src/components/mobile/MobilePanel.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useRef, useState} from "react";
+import React, {memo, useEffect, useRef, useState} from "react";
 import {Altair} from "../altair/Altair";
 import ControlTray from "../control-tray/ControlTray";
 import './MobilePanel.scss'
@@ -6,11 +6,28 @@ import cn from "classnames";
 
 const MobilePanel: React.FC = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
+    const sampleVideoRef = useRef<HTMLVideoElement>(null);
     const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
+    const sampleVideos = useRef([
+        {src: "https://rplus-global-aws.rplushealth.com/rplus-migrate/video_original/zh_CN/157197395955637.mp4", name: "High-legged running"},  // 原地高抬腿跑
+        {src: "https://rplus-global-aws.rplushealth.com/rplus-migrate/video_original/zh_CN/1200329678909476864.mp4", name: "Bend over and touch your toes"},    // 俯身摸脚尖
+        {src: "https://rplus-global-aws.rplushealth.com/rplus-migrate/video_original/zh_CN/160424145273675776.mp4", name: "Standing contralateral elbow and knee touch"}, // 站立位对侧肘膝
+    ])
+
+    useEffect(()=> {
+       if (sampleVideoRef.current) {
+           if (videoStream) {
+               sampleVideoRef.current.play();
+           } else {
+               sampleVideoRef.current.pause();
+           }
+       }
+    });
+
     return (
         <div className="mobile-panel">
+            <Altair/>
             <div className="video-area">
-                <Altair/>
                 <video
                     className={cn("stream", {
                         hidden: !videoRef.current || !videoStream,
@@ -19,6 +36,19 @@ const MobilePanel: React.FC = () => {
                     autoPlay
                     playsInline
                 />
+            </div>
+            <div className="sample-video">
+                <video
+                    className={cn("stream", {
+                        hidden: !videoRef.current || !videoStream,
+                    })}
+                    autoPlay
+                    loop
+                    muted
+                    width="100%"
+                    height="100%"
+                    src={sampleVideos.current[0].src}
+                    ref={sampleVideoRef}/>
             </div>
             <ControlTray videoRef={videoRef} supportsVideo={true} onVideoStreamChange={setVideoStream}></ControlTray>
         </div>
